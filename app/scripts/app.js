f5b3b81687586039e26c0f37a2938817ceeddb78
sebas663 <sebastian_escuderoh@hotmail.com>
2016-04-11 23:49:04 -0300

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name infoGastosApp
+ * @description
+ * # infoGastosApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('infoGastosApp', [
+    'ngAnimate'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name infoGastosApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the infoGastosApp
+ */
+angular.module('infoGastosApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
